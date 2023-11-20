9e8af6714b8be44121f9c26ed6b3ad2a0a484e36
arunkarunanithi <148535629+arunkarunanithi@users.noreply.github.com>
2023-11-20 15:14:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
 resource "aws_instance" "example" {
-    ami           = "ami-00744e52917f35c40"  # Specify an appropriate AMI ID
+    ami           = "ami-0e83be366243f524a"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
 }
